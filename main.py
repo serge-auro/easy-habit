b48c6bc518fc67d8fa97cc74e68fa5acb3e53163
Anashkin Aleksei <109337222+NewalexOA@users.noreply.github.com>
2024-04-21 12:11:31 +0300

    add keyboard and comments

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import telebot
+from telebot import types
+
+bot = telebot.TeleBot('TOKEN')  # Инициализация бота / Initialize the bot
+start_message = 'Привет! Я помощник, который поможет вам отслеживать и поддерживать свои ежедневные привычки.'
+help_message = 'Здесь будет список доступных команд.'
 
 
-bot = telebot.TeleBot('TOKEN')
+# Создание универсальной клавиатуры / Create a universal keyboard
+def create_keyboard():
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)  # Создание объекта клавиатуры / Create keyboard object
+    button_add = types.KeyboardButton('Добавить')  # Кнопка добавления / Add button
+    button_del = types.KeyboardButton('Удалить')  # Кнопка удаления / Delete button
+    button_list = types.KeyboardButton('Список')  # Кнопка списка / List button
+    button_stats = types.KeyboardButton('Статистика')  # Кнопка статистики / Statistics button
+    keyboard.add(button_add, button_del, button_list, button_stats)  # Добавление кнопок на клавиатуру / Add buttons to the keyboard
+    return keyboard
 
-
+# Обработчик команды "start" / Handler for "start" command
 @bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, 'Привет! Я помощник, который поможет вам отслеживать и поддерживать свои ежедневные привычки.')
+def start_message(message):
+    keyboard = create_keyboard()  # Получение клавиатуры / Get the keyboard
+    # Отправка приветственного сообщения с клавиатурой / Send a welcome message with the keyboard
+    bot.send_message(message.chat.id, start_message, reply_markup=keyboard)
 
-
+# Обработчик команды "help" / Handler for "help" command
 @bot.message_handler(commands=['help'])
 def help(message):
-    bot.send_message(message.chat.id, 'Здесь будет список доступных команд.')
+    keyboard = create_keyboard()
+    bot.send_message(message.chat.id, help_message, reply_markup=keyboard)
 
-
-bot.polling(none_stop=True)
+bot.polling(none_stop=True)  # Запуск бота / Start the bot
