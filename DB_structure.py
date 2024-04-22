b9876a1b703a76f72dc63eec811402085e073d87
Buddyroo <165296322+Buddyroo@users.noreply.github.com>
2024-04-22 12:51:13 -0300

    Add files via upload

---
+++ DB_structure.py
@@ -0,0 +1,54 @@
+import sqlite3
+
+import sqlite3
+
+
+def init_db():
+    conn = sqlite3.connect('easy_habit.db')
+    cur = conn.cursor()
+
+    # Включаем поддержку внешних ключей
+    cur.execute("PRAGMA foreign_keys = ON")
+
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS users
+            (id INTEGER PRIMARY KEY,
+             active BOOLEAN NOT NULL DEFAULT 1,
+             creation_date DATE)
+    ''')
+
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS habit
+            (id INTEGER PRIMARY KEY autoincrement NOT NULL,
+             name TEXT NOT NULL,
+             description TEXT)
+    ''')
+
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS user_habit
+            (id INTEGER PRIMARY KEY autoincrement NOT NULL,
+             user_id INTEGER,
+             habit_id INTEGER,
+             active BOOLEAN NOT NULL DEFAULT 0,
+             frequency_name TEXT CHECK(frequency_name IN ('ежедневно', 'еженедельно', 'ежемесячно')),
+             frequency_count INTEGER,
+             FOREIGN KEY (user_id) REFERENCES users(id),
+             FOREIGN KEY (habit_id) REFERENCES habit(id))
+    ''')
+
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS user_habit_history
+            (id INTEGER PRIMARY KEY autoincrement NOT NULL,
+             user_id INTEGER,
+             habit_id INTEGER,
+             mark_date DATE,
+             mark_count INTEGER,
+             FOREIGN KEY (user_id) REFERENCES users(id),
+             FOREIGN KEY (habit_id) REFERENCES habit(id))
+    ''')
+
+    conn.commit()
+    conn.close()
+
+
+
